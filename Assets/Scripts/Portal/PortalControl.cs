5d8e1cb3912531b7aee6012d26e7af4bebb1814e
Gregorius Albeto Beda Yehova <133333252+C3L4N4@users.noreply.github.com>
2024-12-06 23:50:27 +0700

    Updated Portal & Tweaked Animations

--- Assets/Scripts/Portal/PortalControl.cs
+++ Assets/Scripts/Portal/PortalControl.cs
@@ -2,30 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PortalMask : MonoBehaviour
+public class PortalControl : MonoBehaviour
 {
+    private PortalToggle portalToggle;
     public Player player;
 
     void Start()
     {
+        portalToggle = GameObject.FindObjectOfType<PortalToggle>();
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
-    }
-
-    // Player is inside the portal when the player's collider enters the portal's collider
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if(other.gameObject.CompareTag("Player"))
-        {
-            player.isInPortal = true;
-        }
     }
 
     // Player is outside the portal when the player's collider exits the portal's collider
     private void OnTriggerExit2D(Collider2D other)
     {
-        if(other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player"))
         {
-            player.isInPortal = false;
+            // Close the portal when the player exits
+            portalToggle.ClosePortal();
         }
     }
 }
