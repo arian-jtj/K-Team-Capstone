68ed26141f4415b10095ca6eca5241035d65adc1
VinHas2 <157064224+VinHas2@users.noreply.github.com>
2024-12-13 00:20:46 +0700

    Added Pause Menu

---
+++ Assets/Scripts/Misc/PauseManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject musicOnButton;
+    [SerializeField] private GameObject musicOffButton;
+    [SerializeField] private SceneController MainMenuScene;
+
+    public void OpenPauseMenu()
+    {
+        Debug.Log("Bisa");
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void ResumeButton()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void TurnOnMusicButton()
+    {
+        musicOffButton.SetActive(false);
+        musicOnButton.SetActive(true);
+        Debug.Log("TURNON");
+    }
+
+    public void TurnOffMusicButton()
+    {
+        musicOnButton.SetActive(false);
+        musicOffButton.SetActive(true);
+        Debug.Log("TURNOFF");
+    }
+
+    public void GoToMainMenuButton()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
